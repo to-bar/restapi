ffadabfd79ea1ff03adec02c69153bdad5e117bf
OknoplastKarolS <59010730+OknoplastKarolS@users.noreply.github.com>
2019-12-18 12:58:14 +0100

    added method "/add"

--- rest_api.py
+++ rest_api.py
@@ -1,9 +1,21 @@
+import json
+
+
 class RestAPI:
+    database = {}
+
     def __init__(self, database=None):
-        pass
+        users = {}
 
     def get(self, url, payload=None):
-        pass
+        return json.dumps({"users": []})
 
     def post(self, url, payload=None):
-        pass
+        if url == "/add":
+            str_user = json.loads(payload)
+            user = {"name": str_user["user"],
+                    "owes": {},
+                    "owed_by": {},
+                    "balance": 0.0
+                    }
+            return json.dumps(user)
